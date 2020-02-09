bc9899eb9683401f619f042555e237c01eb2a0b5
Igor <GI_Something@protonmail.com>
2020-02-09 17:59:28 +0200

    refactor: Make code more maintainable

--- src/converter/Main.kt
+++ src/converter/Main.kt
@@ -7,20 +7,47 @@ fun main() {
     val number = input[0].toDouble()
     val type = input[1].toLowerCase()
 
-    val output = when (type) {
-        "m", "meter", "meters" -> "$number meter${s(number)} is $number meter${s(number)}"
-        "km", "kilometer", "kilometers" -> "$number kilometer${s(number)} is ${number * 1000} meter${s(number * 1000)}"
-        "cm", "centimeter", "centimeters" -> "$number centimeter${s(number)} is ${number * 0.01} meter${s(number * 0.01)}"
-        "mm", "millimeter", "millimeters" -> "$number millimeter${s(number)} is ${number * 0.001} meter${s(number * 0.001)}"
-        "mi", "mile", "miles" -> "$number mile${s(number)} is ${number * 1609.35} meter${s(number * 1609.35)}"
-        "yd", "yard", "yards" -> "$number yard${s(number)} is ${number * 0.9144} meter${s(number * 0.9144)}"
-        "ft", "foot", "feet" -> "$number f${if (number == 1.0) "oo" else "ee"}t is ${number * 0.3048} meter${s(number)}"
-        "in", "inch", "inches" -> "$number inche${s(number)} is ${number * 0.0254} meter${s(number * 0.0254)}"
+    val meters = toMeters(number, type)
+    if (meters == -1.0) {
+        println("Incorrect input")
+        return
+    }
+    print(createOutputString(type, number, "meter", meters))
+}
 
+fun toMeters(number: Double, type: String): Double {
+    return when (type) {
+        "m", "meter", "meters" -> number
+        "km", "kilometer", "kilometers" -> number * 1000
+        "cm", "centimeter", "centimeters" -> number * 0.01
+        "mm", "millimeter", "millimeters" -> number * 0.001
+        "mi", "mile", "miles" -> number * 1609.35
+        "yd", "yard", "yards" -> number * 0.9144
+        "ft", "foot", "feet" -> number * 0.3048
+        "in", "inch", "inches" -> number * 0.0254
+        else -> -1.0
+    }
+}
+
+fun createOutputString(inType: String, inValue: Double, convType: String, convValue: Double): String {
+    val iType = convertTypeName(inType, inValue)
+    val cType = convertTypeName(convType, convValue)
+
+    return "$inValue $iType is $convValue $cType"
+}
+
+fun convertTypeName(typeName: String, value: Double): String {
+    return when (typeName) {
+        "m", "meter", "meters" -> "meter${s(value)}"
+        "km", "kilometer", "kilometers" -> "kilometer${s(value)}"
+        "cm", "centimeter", "centimeters" -> "centimeter${s(value)}"
+        "mm", "millimeter", "millimeters" -> "millimeter${s(value)}"
+        "mi", "mile", "miles" -> "mile${s(value)}"
+        "yd", "yard", "yards" -> "yard${s(value)}"
+        "ft", "foot", "feet" -> if (value == 1.0) "foot" else "feet"
+        "in", "inch", "inches" -> if (value == 1.0) "inch" else "inches"
         else -> "Unknown type"
     }
-
-    print(output)
 }
 
 fun s(number: Double): String = if (number == 1.0) "" else "s"
