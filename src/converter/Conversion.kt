399f30b790b9ba58170c7006414dc1a8cb49179b
Igor <GI_Something@protonmail.com>
2020-02-11 22:42:35 +0200

    refactor: Move toMeters, metersTo, toGrams, gramsTo to Conversion.kt

---
+++ src/converter/Conversion.kt
@@ -0,0 +1,53 @@
+package converter
+
+import converter.Unit.*
+
+fun toMeters(value: Double, unit: Unit): Double {
+    return when (unit) {
+        METER -> value
+        KILOMETER -> value * 1000
+        CENTIMETER -> value * 0.01
+        MILLIMETER -> value * 0.001
+        MILE -> value * 1609.35
+        YARD -> value * 0.9144
+        FOOT -> value * 0.3048
+        INCH -> value * 0.0254
+        else -> -1.0
+    }
+}
+
+fun metersTo(value: Double, unit: Unit): Double {
+    return when (unit) {
+        METER -> value
+        KILOMETER -> value / 1000.0
+        CENTIMETER -> value / 0.01
+        MILLIMETER -> value / 0.001
+        MILE -> value / 1609.35
+        YARD -> value / 0.9144
+        FOOT -> value / 0.3048
+        INCH -> value / 0.0254
+        else -> -1.0
+    }
+}
+
+fun toGrams(value: Double, unit: Unit): Double {
+    return when (unit) {
+        GRAM -> value
+        KILOGRAM -> value * 1000
+        MILLIGRAM -> value * 0.001
+        POUND -> value * 453.592
+        OUNCE -> value * 28.3495
+        else -> -1.0
+    }
+}
+
+fun gramsTo(value: Double, unit: Unit): Double {
+    return when (unit) {
+        GRAM -> value
+        KILOGRAM -> value / 1000.0
+        MILLIGRAM -> value / 0.001
+        POUND -> value / 453.592
+        OUNCE -> value / 28.3495
+        else -> -1.0
+    }
+}
